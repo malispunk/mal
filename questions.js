5cc191c4d823e54356ca680e924616d3696393bb
malispunk <137846110+malispunk@users.noreply.github.com>
2023-06-26 22:17:25 -0300

    upload site

---
+++ questions.js
@@ -0,0 +1,122 @@
+let questions = [
+	{
+	numb: 1,
+	question: "Como é o nome da soberana do Reino Doce?",
+	answer: "C) Princesa Jujuba",
+	options: [
+		"A) Princesa Lolipop",
+		"B) Princesa Marshmallow",
+		"C) Princesa Jujuba",
+		"D) Princesa Cupcake"
+		]
+	},
+	
+	{
+	numb: 2,
+	question: "Quem pede BMO em casamento?",
+	answer: "B) Bolha",
+	options: [
+		"A) Futebol",
+		"B) Bolha",
+		"C) Ronnie, o rato",
+		"D) Canelinha"
+		]
+	},
+	
+	{
+	numb: 3,
+	question: "Onde a Princesa de Fogo ficou presa durante a infância?",
+	answer: "A) Em uma lâmpada",
+	options: [
+		"A) Em uma lâmpada",
+		"B) Em um vulcão",
+		"C) No Sol",
+		"D) Na lareira azul"
+		]
+	},
+	
+	{
+	numb: 4,
+	question: "Onde Joshua e Margaret acharam o Finn bebê?",
+	answer: "A) Na floresta",
+	options: [
+		"A) Na floresta",
+		"B) Na estrada",
+		"C) Na cabana",
+		"D) No rio gelado"
+		]
+	},
+	
+	{
+	numb: 5,
+	question: "Quantos filhos o Jake teve com a Lady Íris?",
+	answer: "C) Cinco",
+	options: [
+		"A) Seis",
+		"B) Dois",
+		"C) Cinco",
+		"D) Três"
+		]
+	},
+	
+	{
+	numb: 6,
+	question: "Qual instrumento musical tocado por Lemonhope que agradou aos Limãograbs?",
+	answer: "D) Flauta",
+	options: [
+		"A) Violão",
+		"B) Harpa",
+		"C) Tambor",
+		"D) Flauta"
+		]
+	},
+	
+	{
+	numb: 7,
+	question: "O que Simon Petrikov deu para Marceline na infância?",
+	answer: "B) Um ursinho",
+	options: [
+		"A) Uma guitarrao",
+		"B) Um ursinho",
+		"C) Uma bicicleta",
+		"D) Um amuleto"
+		]
+	},
+	
+	{
+	numb: 8,
+	question: "Qual o primeiro personagem a sumir no episódio Sustos?",
+	answer: "C) Canelinha",
+	options: [
+		"A) BMO",
+		"B) Princesa Caroço",
+		"C) Canelinha",
+		"D) Princesa Jujuba"
+		]
+	},
+	
+	{
+	numb: 9,
+	question: "Qual o idioma utilizado pela Lady Íris?",
+	answer: "A) Coreano",
+	options: [
+		"A) Coreano",
+		"B) Japonês",
+		"C) Chinês",
+		"D) Tailandês"
+		]
+	},
+	
+	{
+	numb: 10,
+	question: "Qual o nome da tribo da Susana Forte?",
+	answer: "C) Hyoomanos",
+	options: [
+		"A) Hinumanus",
+		"B) Hoomanys",
+		"C) Hyoomanos",
+		"D) Hoomanydis"
+		]
+		
+	 },
+];
